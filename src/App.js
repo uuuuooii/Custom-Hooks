4b3a1a45eb51f2b416691c15a39d56e7c375c38c
neo@zillinks.com <keunhey0107@gmail.com>
2022-08-03 01:12:38 +0900

    custom-hooks

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Example1 from "./components/Example1";
+import Example2 from "./components/Example2";
+import Example3 from "./components/Example3";
+import Example4 from "./components/Example4";
+import Example5 from "./components/Example5";
+import useWindowWidth from "./hoosk/useWindowWidth";
+
+function App() {
+  const width = useWindowWidth();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <Example1 />
+        <Example2 />
+        <Example3 />
+        <Example4 /> */}
+        <Example5 />
+        {width}
+      </header>
+    </div>
+  );
+}
+
+export default App;
